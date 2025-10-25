0fccb4bf2eb8a1d57752a20e8d691b4fcb879b97
장성원 <jjang6251@gmail.com>
2025-10-25 18:37:17 +0900

    test: 자동차 이름 파싱 케이스 수정

--- src/test/java/racingcar/CarNameParserTest.java
+++ src/test/java/racingcar/CarNameParserTest.java
@@ -1,32 +1,21 @@
 package racingcar;
 
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.Race;
+import racingcar.service.CarService;
+import racingcar.service.CarServiceImpl;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class CarNameParserTest {
-    @DisplayName("쉼표(,) 이외의 구분자를 사용하면 예외가 발생한다.")
-    @Test
-    void 쉼표_구분자_성공_케이스() {
-        String input = "pobi,woni";
+    private CarService carService;
 
-        List<String> result = CarNameParser.parse(input);
-
-        assertThat(result)
-                .hasSize(2)
-                .containsExactly("pobi", "woni");
-    }
-
-    @DisplayName("쉼표(,) 이외의 구분자를 사용하면 예외가 발생한다.")
-    @Test
-    void 쉼표_이외의_구분자_예외발생() {
-        String input = "pobi:woni";
-
-        assertThatThrownBy(() -> CarNameParser.parse(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("separator err");
+    @BeforeEach
+    void setUp() {
+        carService = new CarServiceImpl();
     }
 
     @DisplayName("자동차 이름이 5자를 초과하면 예외 발생.")
@@ -34,7 +23,8 @@ public class CarNameParserTest {
     void 자동차_이름_5자_초과() {
         String input = "pobibi,woniwoni";
 
-        assertThatThrownBy(() -> CarNameParser.parse(input))
+
+        assertThatThrownBy(() -> carService.createRace(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("car name length exceeded");
     }
@@ -42,14 +32,10 @@ public class CarNameParserTest {
     @DisplayName("빈 문자열 또는 공백만 입력한 경우 예외 발생")
     @Test
     void 빈문자열_공백_예외발생() {
-        String input1 = "";
-        String input2 = "   ";
+        String input = "";
 
-        assertThatThrownBy(() -> CarNameParser.parse(input1))
+        assertThatThrownBy(() -> carService.createRace(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("empty string err");
-        assertThatThrownBy(() -> CarNameParser.parse(input2))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("blank field err");
     }
 }
