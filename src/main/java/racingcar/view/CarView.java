8c8b7130105bbb99ca20f0b81ff4c8492f7f2581
장성원 <jjang6251@gmail.com>
2025-10-25 19:11:16 +0900

    feat: 시도 횟수 파싱 로직 구현

--- src/main/java/racingcar/view/CarView.java
+++ src/main/java/racingcar/view/CarView.java
@@ -7,4 +7,16 @@ public class CarView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
+
+    public int getCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String s = Console.readLine();
+        try {
+            int n = Integer.parseInt(s);
+            if(n <= 0) throw new IllegalArgumentException("The number of attempts must be a positive integer.");
+            return n;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("should enter a number");
+        }
+    }
 }
