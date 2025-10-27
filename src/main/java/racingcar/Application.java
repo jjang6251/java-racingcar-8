d65de9f386959222ef2207c676afbc0f4bd65779
장성원 <jjang6251@gmail.com>
2025-10-27 23:14:25 +0900

    faet: Application main 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,10 +1,17 @@
 package racingcar;
 
+import racingcar.controller.CarController;
+import racingcar.domain.RandomMoveStrategy;
+import racingcar.service.CarService;
+import racingcar.service.CarServiceImpl;
+import racingcar.view.CarView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String input;
-        int ret;
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        CarView carView = new CarView();
+        CarService carService = new CarServiceImpl(new RandomMoveStrategy());
+        var controller = new CarController(carView, carService);
+        controller.run();
     }
 }
